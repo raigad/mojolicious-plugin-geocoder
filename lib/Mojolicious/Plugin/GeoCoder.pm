22c057af03cef531dffee7a800e8d701271a4a45
Rohit <rohitdeshmukh69@gmail.com>
2014-12-06 13:49:26 +0000

    Added META.yml and META.json files

--- lib/Mojolicious/Plugin/GeoCoder.pm
+++ lib/Mojolicious/Plugin/GeoCoder.pm
@@ -5,7 +5,7 @@ use warnings FATAL => 'all';
 use Mojo::Base 'Mojolicious::Plugin';
 use Geo::Coder::Google;
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 sub register {
     my ($self,$app,$rh_conf) = @_;
@@ -60,7 +60,7 @@ Mojolicious::Plugin::GeoCoder - Plugin for geocoding and reverse geocoding featu
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
